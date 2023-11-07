bca4abd12e1023132ab7e6b68749e83e59ab6b5c
LudaTverday <Ludachka22@gmail.com>
2023-11-07 09:51:21 +0200

    adding annotations for put, post, delete and get data

--- src/main/java/telran/spring/controller/GreetingsController.java
+++ src/main/java/telran/spring/controller/GreetingsController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 import telran.spring.service.GreetingService;
+import telran.spring.service.IdName;
 
 @RestController
 @RequestMapping("greetings")
@@ -15,5 +16,23 @@ public class GreetingsController {
 	String getGreetings(@PathVariable long id) {
 		return greetingsService.getGreetings(id);
 	}
+	//TODO update following control end point 
+	//methods for HW#57 according to updated service
+	@PostMapping
+	String addName(@RequestBody IdName idName) {;
+	return greetingsService.addName(idName);
+	}
+	@PutMapping
+	String updateName(@RequestBody IdName idName) {
+		return greetingsService.updateName(idName);
+	}
+	@DeleteMapping("{id}")
+	String deleteName (long id) {
+		return greetingsService.deleteName(id);
+	}
+	//TODO 
+	//getMapping
+	//end point for getting person 
+	//by id and getting persons by city see service
 
 }
--- src/main/java/telran/spring/service/GreetingService.java
+++ src/main/java/telran/spring/service/GreetingService.java
@@ -1,6 +1,19 @@
 package telran.spring.service;
 
+import telran.spring.Person;
+
 public interface GreetingService {
 	String getGreetings(long id);
 
+	// TODO For HW#57
+	// Update and add: 
+	//Person getPerson(long id) if have`t as null
+	//Person addPerson(Person person)
+	
+	//List<Person> getPersonsByCity(String city)
+	//Person deletePerson(long id)
+	//Person updatePerson(Person person)
+	String addName(IdName idName);
+	String deleteName(long id);
+	String updateName(IdName idName);
 }

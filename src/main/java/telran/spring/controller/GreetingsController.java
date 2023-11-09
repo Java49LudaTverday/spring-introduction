21e4fad80d1deb4bcc12084d1e177df67ff2ce67
LudaTverday <Ludachka22@gmail.com>
2023-11-09 11:02:51 +0200

    added logs to methods

--- src/main/java/telran/spring/controller/GreetingsController.java
+++ src/main/java/telran/spring/controller/GreetingsController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.*;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import telran.spring.Person;
 import telran.spring.service.GreetingService;
 import telran.spring.service.IdName;
@@ -13,16 +15,18 @@ import telran.spring.service.IdName;
 @RestController
 @RequestMapping("greetings")
 @RequiredArgsConstructor
+@Slf4j
 public class GreetingsController {
 	final GreetingService greetingsService;
 	//TODO update following control end point 
 	//methods for HW#57 according to updated service
 	@PostMapping
-	Person addPerson (@RequestBody Person person) {
+	Person addPerson (@RequestBody @Valid Person person) {
+		log.debug("method: add person, received {}", person);
 		return greetingsService.addPerson(person);
 	}
 	@PutMapping
-	Person updatePerson (@RequestBody Person person) {
+	Person updatePerson (@RequestBody @Valid Person person) {
 		return greetingsService.updatePerson(person);
 	}
 	@GetMapping("/getPerson/{id}")
@@ -35,11 +39,18 @@ public class GreetingsController {
 	}
 	@GetMapping("/getByCity/{city}")
 	List<Person> getPersosByCity (@PathVariable String city){
-		return greetingsService.getPersonsByCity(city);
+		List<Person> result =  greetingsService.getPersonsByCity(city);
+		if(result.isEmpty()) {
+			log.warn("received empty list for city: {}", city);
+		} else {
+			log.trace("result is {}", result);
+		}
+		return result;
 	}	
 
 	@GetMapping("{id}")
 	String getGreetings(@PathVariable long id) {
+		log.debug("received id {}", id);
 		return greetingsService.getGreetings(id);
 	}
 	

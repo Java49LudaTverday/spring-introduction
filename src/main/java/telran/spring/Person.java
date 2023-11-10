12ca61188c80a7df717f73aaee6220e3e17927ab
LudaTverday <Ludachka22@gmail.com>
2023-11-10 13:02:30 +0200

    adding validation for fields of Person

--- src/main/java/telran/spring/Person.java
+++ src/main/java/telran/spring/Person.java
@@ -1,14 +1,12 @@
 package telran.spring;
 
-import java.util.Objects;
-
 import jakarta.validation.constraints.*;
 
 
-public record Person(long id,@Pattern(regexp = "[A-Z][a-z]{2,}") String name,
-		@NotEmpty String city,/*TODO validation annotation*/ String email,
-		/*TODO Israel mobile phone validation*/  String phone) {
-
-	
+public record Person( long id,@Pattern(regexp = "[A-Z][a-z]{2,}") String name,
+		@NotEmpty String city,/*TODO validation annotation*/@Email(message = "Email is not valid", 
+		regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$") String email,
+		/*TODO Israel mobile phone validation*/ @Pattern(regexp = "^\\+?972-?\\d{2,3}-\\d{7}$", 
+		message = "Please enter a valid Israel phone number in format +972-XXX-XXXXXXX") String phone) {
 
 }

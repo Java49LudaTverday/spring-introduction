03a52c8cec82619730bff194a73c28d704d2cdd2
LudaTverday <Ludachka22@gmail.com>
2023-11-10 13:01:45 +0200

    adding logs for methods

--- src/main/java/telran/spring/controller/GreetingsController.java
+++ src/main/java/telran/spring/controller/GreetingsController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import telran.exceptions.NotFoundException;
 import telran.spring.Person;
 import telran.spring.service.GreetingService;
 import telran.spring.service.IdName;
@@ -18,8 +19,7 @@ import telran.spring.service.IdName;
 @Slf4j
 public class GreetingsController {
 	final GreetingService greetingsService;
-	//TODO update following control end point 
-	//methods for HW#57 according to updated service
+	
 	@PostMapping
 	Person addPerson (@RequestBody @Valid Person person) {
 		log.debug("method: add person, received {}", person);
@@ -27,18 +27,27 @@ public class GreetingsController {
 	}
 	@PutMapping
 	Person updatePerson (@RequestBody @Valid Person person) {
+		log.debug("method: update person, received {}", person);
 		return greetingsService.updatePerson(person);
 	}
 	@GetMapping("/getPerson/{id}")
 	Person getPerson (@PathVariable long id) {
-		return greetingsService.getPerson(id);
+		log.debug("method: get person, received {}", id);
+		Person person = greetingsService.getPerson(id);
+		if(person == null) {
+			log.warn("method: get person, not found the person with id {}", id);
+			throw new NotFoundException(String.format("Not found the person with id %d", id));
+		}
+		return person;
 	}
 	@DeleteMapping("{id}")
 	Person deletePerson (@PathVariable long id) {
+		log.debug("method: delete person, received {}", id);
 		return greetingsService.deletePerson(id);
 	}
 	@GetMapping("/getByCity/{city}")
 	List<Person> getPersosByCity (@PathVariable String city){
+		log.debug("method: get person by city, received {}", city);
 		List<Person> result =  greetingsService.getPersonsByCity(city);
 		if(result.isEmpty()) {
 			log.warn("received empty list for city: {}", city);
@@ -50,7 +59,7 @@ public class GreetingsController {
 
 	@GetMapping("{id}")
 	String getGreetings(@PathVariable long id) {
-		log.debug("received id {}", id);
+		log.debug("method: get greetings, received id {}", id);
 		return greetingsService.getGreetings(id);
 	}
 	
--- src/main/java/telran/spring/service/GreetingsServiceImpl.java
+++ src/main/java/telran/spring/service/GreetingsServiceImpl.java
@@ -3,15 +3,19 @@ package telran.spring.service;
 import java.util.*;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
 import telran.exceptions.NotFoundException;
 import telran.spring.Person;
+import telran.spring.controller.GreetingsController;
 
 @Service
+@Slf4j
 public class GreetingsServiceImpl implements GreetingService {
 	Map<Long, Person> persons = new HashMap<>();
 	
 	@Override
 	public String getGreetings(long id) {
+		log.debug("method: get greetings, received {}", id);
 		Person person = persons.get(id);
 		String name = person.name();
 		return "Hello " + name;
@@ -19,12 +23,13 @@ public class GreetingsServiceImpl implements GreetingService {
 
 	@Override
 	public Person getPerson(long id) {
-
+		log.debug("method: get Person, received {}", id);
 		return persons.get(id);
 	}
 	
 	@Override
 	public Person addPerson(Person person) {
+		log.debug("method: add Person, received {}", person);
 		Long id = person.id();
 		if(persons.containsKey(id)) {
 			throw new IllegalStateException(String.format("person with id %d already exists", id));
@@ -35,12 +40,13 @@ public class GreetingsServiceImpl implements GreetingService {
 
 	@Override
 	public List<Person> getPersonsByCity(String city) {
-
+		log.debug("method: get Person by city, received {}", city);
 		return persons.values().stream().filter(p -> p.city().equals(city)).toList();
 	}
 
 	@Override
 	public Person deletePerson(long id) {
+		log.debug("method: delete Person, received {}", id);
 		if(!persons.containsKey(id)) {
 			throw new NotFoundException(String.format("person with id %d doesn't exist", id));
 		}
@@ -50,6 +56,7 @@ public class GreetingsServiceImpl implements GreetingService {
 
 	@Override
 	public Person updatePerson(Person newPerson) {
+		log.debug("method: update Person, received {}", newPerson);
 		long id = newPerson.id();
 		if(!persons.containsKey(id)) {
 			throw new NotFoundException(String.format("person with id %d doesn't exist", id));

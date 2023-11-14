52219524e8e9049cf8f2a6ae715f8bce117fb24b
LudaTverday <Ludachka22@gmail.com>
2023-11-14 15:51:12 +0200

    validation for field id

--- src/main/java/telran/spring/Person.java
+++ src/main/java/telran/spring/Person.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import jakarta.validation.constraints.*;
 
 
-public record Person ( long id,@Pattern(regexp = "[A-Z][a-z]{2,}") String name,
+public record Person ( @NotNull long id,@Pattern(regexp = "[A-Z][a-z]{2,}") String name,
 		@NotEmpty String city, @Email @NotEmpty String email,
 		@Pattern(regexp = "(\\+972-?|0)5\\d-?\\d{7}",
 		message="not Israel mobile phone") @NotEmpty  String phone)implements Serializable {

7efb33479bbe9110ef72ff214c488d21eebf6063
LudaTverday <Ludachka22@gmail.com>
2023-11-14 13:10:14 +0200

    adding implements Serializable for saving and restoring

--- src/main/java/telran/spring/Person.java
+++ src/main/java/telran/spring/Person.java
@@ -1,12 +1,13 @@
 package telran.spring;
 
+import java.io.Serializable;
+
 import jakarta.validation.constraints.*;
 
 
-public record Person( long id,@Pattern(regexp = "[A-Z][a-z]{2,}") String name,
-		@NotEmpty String city,/*TODO validation annotation*/@Email(message = "Email is not valid", 
-		regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$") String email,
-		/*TODO Israel mobile phone validation*/ @Pattern(regexp = "^\\+?972-?\\d{2,3}-\\d{7}$", 
-		message = "Please enter a valid Israel phone number in format +972-XXX-XXXXXXX") String phone) {
+public record Person ( long id,@Pattern(regexp = "[A-Z][a-z]{2,}") String name,
+		@NotEmpty String city, @Email @NotEmpty String email,
+		@Pattern(regexp = "(\\+972-?|0)5\\d-?\\d{7}",
+		message="not Israel mobile phone") @NotEmpty  String phone)implements Serializable {
 
 }

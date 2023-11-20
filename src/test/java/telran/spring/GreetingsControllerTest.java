108a75a0a6c044c85fb2d0ca243d92db26027cd5
LudaTverday <Ludachka22@gmail.com>
2023-11-20 12:08:13 +0200

    all tests for GreetingsController

--- src/test/java/telran/spring/GreetingsControllerTest.java
+++ src/test/java/telran/spring/GreetingsControllerTest.java
@@ -6,6 +6,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -15,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import telran.exceptions.NotFoundException;
 import telran.spring.controller.GreetingsController;
 import telran.spring.service.GreetingService;
 
@@ -25,7 +28,9 @@ record PersonIdString(String id, String name, String city, String mail, String p
 
 @WebMvcTest
 public class GreetingsControllerTest {
-    @Autowired //this annotation allows dependency injection inside following field 
+    private static final String GET_GREETINGS_RETURN = "Hello";
+	private static final String PERSON_NOT_FOUND = "person not found";
+	@Autowired //this annotation allows dependency injection inside following field 
 	GreetingsController controller;
     @MockBean
     GreetingService service;
@@ -124,30 +129,72 @@ public class GreetingsControllerTest {
      }
      @Test
      void updatePersonTest() throws Exception {
-    	 mockMvc.perform(put("http://localhost:8080/greetings")
+    	 when(service.updatePerson(personNormalUpdated)).thenReturn(personNormalUpdated);
+    	 String personJSON = objectMapper.writeValueAsString(personNormalUpdated);
+    	String response = mockMvc.perform(put("http://localhost:8080/greetings")
     			 .contentType(MediaType.APPLICATION_JSON)
     			 .content(objectMapper.writeValueAsString(personNormalUpdated)))
-    	 .andDo(print()).andExpect(status().isOk());
+    	 .andDo(print()).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+    	assertEquals(personJSON, response);
      }
      @Test
      void getPersonTest() throws Exception {
-    	 mockMvc.perform(get("http://localhost:8080/greetings/id/123"))
-    	 .andDo(print()).andExpect(status().isOk());
+    	 long id = 123;
+    	 long idNotExist = 124;
+    	 when(service.getPerson(id)).thenReturn(personNormal);
+    	 when(service.getPerson(idNotExist)).thenReturn(null);
+    	 String personJSON = objectMapper.writeValueAsString(personNormal);
+    	 String response = getPersonResponse(id);
+    	 assertEquals(personJSON,response);
+    	 String responseEmpty = getPersonResponse(idNotExist);
+    	 assertEquals("",responseEmpty);
      }
+     
+     private String getPersonResponse(long id) throws Exception {
+ 		String response = mockMvc.perform(get("http://localhost:8080/greetings/id/" + id))
+     	 .andDo(print()).andExpect(status().isOk())
+     	 .andReturn().getResponse().getContentAsString();
+ 		return response;
+ 	}
      @Test
      void getGreetingsTest() throws Exception {
-    	 mockMvc.perform(get("http://localhost:8080/greetings/123"))
-    	 .andDo(print()).andExpect(status().isOk());
+    	 long id = 123;
+    	 when(service.getGreetings(id)).thenReturn(GET_GREETINGS_RETURN);
+    	 String response = mockMvc.perform(get("http://localhost:8080/greetings/123"))
+    	 .andDo(print()).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+    	 assertEquals(GET_GREETINGS_RETURN, response);
      }
      @Test
      void getPersonsByCityTest() throws Exception {
-    	 mockMvc.perform(get("http://localhost:8080/greetings/city/Rehovot"))
-    	 .andDo(print()).andExpect(status().isOk());
+    	 String city = "Rehovot";
+    	 when(service.getPersonsByCity(city)).thenReturn(List.of(personNormal, personNormalUpdated));
+    	 Person[] expected = {
+     			personNormal,
+     			personNormalUpdated
+     	};
+    	String response =  mockMvc.perform(get("http://localhost:8080/greetings/city/Rehovot"))
+    	 .andDo(print()).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+    	Person[] actual = objectMapper.readValue(response, Person[].class);
+		assertArrayEquals(expected, actual);
      }
      @Test
-     void deletePersonTest() throws Exception {
-    	 mockMvc.perform(delete("http://localhost:8080/greetings/123"))
-    	 .andDo(print()).andExpect(status().isOk());
+     void deletePersonNormalTest() throws Exception {
+    	 long id = 123;
+    	 when(service.deletePerson(id)).thenReturn(personNormal);
+    	 String personJSON = objectMapper.writeValueAsString(personNormal);
+    	String response =  mockMvc.perform(delete("http://localhost:8080/greetings/123"))
+    	 .andDo(print()).andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+    	 assertEquals(personJSON, response);
+    	 
+     }
+     @Test
+     void deletePersonNotFoundTest() throws Exception {
+    	 long id = 124;
+    	when(service.deletePerson(id)).thenThrow(new NotFoundException(PERSON_NOT_FOUND));
+    	String response =  mockMvc.perform(delete("http://localhost:8080/greetings/"+id))
+    	 .andDo(print()).andExpect(status().isNotFound()).andReturn().getResponse().getContentAsString();
+    	 assertEquals(PERSON_NOT_FOUND, response);
+    	 
      }
      
 }

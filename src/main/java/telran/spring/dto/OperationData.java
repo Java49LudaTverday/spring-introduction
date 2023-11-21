ff8008cab539d8a2d6ad060791bcbeae30e60c87
LudaTverday <Ludachka22@gmail.com>
2023-11-21 13:22:54 +0200

    data structure for application implementation

--- src/main/java/telran/spring/dto/OperationData.java
+++ src/main/java/telran/spring/dto/OperationData.java
@@ -1,7 +1,8 @@
 package telran.spring.dto;
 import jakarta.validation.constraints.*;
 public record OperationData(@NotEmpty @Pattern( regexp = "[a-z]{3,5}", message="wrong type format")String type,
-		@NotEmpty String operation,
+		@NotEmpty @Pattern(regexp = "[a-zA-Z]*",
+		message="wrong operation format") String operation,
 		String operand1, String operand2) {
 
 }
